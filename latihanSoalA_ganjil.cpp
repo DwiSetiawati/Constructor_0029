1fec75c9c3478ed05eab7fe7d37f8041d59d988d
DwiSetiawati <dwisetwa27@gmail.com>
2025-05-17 16:30:50 +0700

    Latihan soal_Membuat class mahasiswa

--- latihanSoalA_ganjil.cpp
+++ latihanSoalA_ganjil.cpp
@@ -5,3 +5,20 @@ using namespace std;
 class Dosen;
 class Staff;
 class Universitas;
+
+class Mahasiswa
+{
+private:
+    string nama;
+    string NIM;
+    float nilai;
+
+public:
+    Mahasiswa(string n, string nim) : nama(n), NIM(nim), nilai(0.0) {}
+
+    void tampilkanInfo()
+    {
+        cout << "Mahasiswa: " << nama << ", NIM: " << NIM << ", Nilai: " << nilai << endl;
+    }
+    friend class Dosen;
+};
